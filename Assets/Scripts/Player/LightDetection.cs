69995959f995200e23996b4fcc8676ab4718fe1d
edwin-od <edwin.odeimi@lau.edu>
2022-11-17 10:47:32 +0100

    light layers

--- Assets/Scripts/Player/LightDetection.cs
+++ Assets/Scripts/Player/LightDetection.cs
@@ -2,16 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider2D))]
 public class LightDetection : MonoBehaviour
 {
     [SerializeField] LayerMask objectLayer;
     [SerializeField] LayerMask platformLayer;
 
-    private void OnTriggerEnter2D(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log(other.name);
         if (((1 << other.gameObject.layer) & objectLayer) != 0)
         {
             // object enter
+            Debug.Log("Object");
         }
         else if (((1 << other.gameObject.layer) & platformLayer) != 0)
         {
