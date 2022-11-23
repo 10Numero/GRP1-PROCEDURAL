9f6f2acb598661b2596f97d24aa9b0c94fd1b6e5
edwin-od <edwin.odeimi@lau.edu>
2022-11-23 10:50:34 +0100

    implementing end interact in test class

--- Assets/Scripts/ObstacleTest.cs
+++ Assets/Scripts/ObstacleTest.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 
 public class ObstacleTest : LightInteractable
 {
+    
     public override void InteractStart(ColorType color)
     {
         Debug.Log("Obstacle");
         this.color = color;
+    }
+
+    public override void InteractEnd(ColorType color)
+    {
+
     }
 }
